0c2958650714ef3922637fe41a6c9f461a24fa88
Dorian <dorian.fouvez@student.vinci.be>
2021-05-21 23:52:45 +0200

    add Color class

---
+++ backend/sister_site/src/main/java/domaine/color/ColorDTO.java
@@ -0,0 +1,19 @@
+/**
+ * @author Fouvez Dorian.
+ */
+package domaine.color;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(as = ColorImpl.class)
+public interface ColorDTO {
+
+  int getId();
+
+  void setId(int id);
+
+  String getColor();
+
+  void setColor(String color);
+
+}
---
+++ backend/sister_site/src/main/java/domaine/color/ColorImpl.java
@@ -0,0 +1,65 @@
+/**
+ * @author Fouvez Dorian.
+ */
+package domaine.color;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ColorImpl implements ColorDTO {
+
+  private int id;
+  private String color;
+
+  @Override
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  @Override
+  public String getColor() {
+    return color;
+  }
+
+  @Override
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  @Override
+  public String toString() {
+    return "ColorImpl [id=" + id + ", color=" + color + "]";
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + id;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    ColorImpl other = (ColorImpl) obj;
+    if (id != other.id) {
+      return false;
+    }
+    return true;
+  }
+
+}
