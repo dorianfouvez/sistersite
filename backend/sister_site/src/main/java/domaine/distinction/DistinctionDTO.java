ed03dfd66d94f7a9b214cb92967b17ca3803df88
Dorian <dorian.fouvez@student.vinci.be>
2021-05-25 20:43:21 +0200

    add distinction

---
+++ backend/sister_site/src/main/java/domaine/distinction/DistinctionDTO.java
@@ -0,0 +1,19 @@
+/**
+ * @author Fouvez Dorian.
+ */
+package domaine.distinction;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(as = DistinctionImpl.class)
+public interface DistinctionDTO {
+
+  int getId();
+
+  void setId(int id);
+
+  String getName();
+
+  void setName(String name);
+
+}
---
+++ backend/sister_site/src/main/java/domaine/distinction/DistinctionImpl.java
@@ -0,0 +1,65 @@
+/**
+ * @author Fouvez Dorian.
+ */
+package domaine.distinction;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DistinctionImpl implements DistinctionDTO {
+
+  private int id;
+  private String name;
+
+  @Override
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "DistinctionImpl [id=" + id + ", name=" + name + "]";
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + id;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    DistinctionImpl other = (DistinctionImpl) obj;
+    if (id != other.id) {
+      return false;
+    }
+    return true;
+  }
+
+}
