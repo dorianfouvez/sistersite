bac6409a1e794f9c8a1fbe1a01f0b91f73d40213
Dorian <dorian.fouvez@student.vinci.be>
2021-05-25 20:44:30 +0200

    add director

---
+++ backend/sister_site/src/main/java/domaine/director/DirectorDTO.java
@@ -0,0 +1,19 @@
+/**
+ * @author Fouvez Dorian.
+ */
+package domaine.director;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(as = DirectorImpl.class)
+public interface DirectorDTO {
+
+  int getId();
+
+  void setId(int id);
+
+  String getName();
+
+  void setName(String name);
+
+}
